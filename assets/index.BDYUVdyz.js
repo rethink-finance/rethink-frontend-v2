8b944472fc94b586f07574e18af41c86ced50d54
cinquemb <1519017+cinquemb@users.noreply.github.com>
2024-03-14 06:29:38 -0400

    Updates

---
+++ assets/index.BDYUVdyz.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./entry.CIR_0B0R.js";const o={},t={class:"d-flex",style:{width:"100%","flex-direction":"column"}};function s(r,_){return n(),c("div",t," Governance ")}const d=e(o,[["render",s]]);export{d as default};
