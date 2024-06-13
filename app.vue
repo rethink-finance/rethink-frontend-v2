41511b059522a79b52aec985c7c9c8541d3176df
Luka Vuk <luka.vuk0909@gmail.com>
2024-06-13 13:43:26 +0200

    dummy commit

--- app.vue
+++ app.vue
@@ -4,9 +4,11 @@ import { useAccountStore } from "~/store/account.store";
 
 onMounted(() => {
   useAccountStore().web3Onboard = useOnboard();
-  if (useOnboard().alreadyConnectedWallets) useAccountStore().setAlreadyConnectedWallet();
+  if (useOnboard().alreadyConnectedWallets)
+    useAccountStore().setAlreadyConnectedWallet();
 });
 </script>
+<!-- dummy commit -->
 
 <template>
   <NuxtLayout>
