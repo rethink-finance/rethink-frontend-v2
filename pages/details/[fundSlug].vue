f4958cfc2c6d1440b46a04777aae99bbf0bbb584
Luka Vuk <luka.vuk0909@gmail.com>
2024-07-25 22:44:56 +0200

    Add fund chain to fund details URL

--- pages/details/[fundSlug].vue
+++ pages/details/[fundSlug].vue
@@ -52,6 +52,9 @@
     </div>
     <NuxtPage :fund="fund" @update-breadcrumbs="setBreadcrumbItems" />
   </div>
+  <div v-else-if="isSwitchingNetworks" class="w-100 d-flex justify-center">
+    <v-progress-circular indeterminate />
+  </div>
   <div v-else class="d-flex flex-column h-100 align-center">
     <h2 class="mb-2">Fund not found</h2>
     <p class="text-center">
@@ -63,19 +66,25 @@
 
 <script lang="ts" setup>
 import { trimTrailingSlash } from "~/composables/utils";
+import { useAccountStore } from "~/store/account.store";
 import { useFundStore } from "~/store/fund.store";
+import { useToastStore } from "~/store/toast.store";
 import { useWeb3Store } from "~/store/web3.store";
 import type IFund from "~/types/fund";
 import type IRoute from "~/types/route";
 import type BreadcrumbItem from "~/types/ui/breadcrumb";
 
+const accountStore = useAccountStore();
+const toastStore = useToastStore();
 const fundStore = useFundStore();
 const web3Store = useWeb3Store();
 const route = useRoute();
+const router = useRouter();
 const loading = ref(true);
-// fund address is always in the second position of the route
-// e.g. /details/TFD3-0x1234 -> 0x1234
-const [tokenSymbol, fundAddress] = route.path.split("/")[2].split("-");
+const isSwitchingNetworks = ref(false);
+// fund address is always in the third position of the route
+// e.g. /details/0xa4b1-TFD3-0x1234 -> 0x1234
+const [chainId, tokenSymbol, fundAddress] = route.path.split("/")[2].split("-");
 
 onUnmounted(() => {
   fundStore.fund = {} as IFund;
@@ -115,7 +124,7 @@ watch(
 watch(
   () => route.path,
   (newPath) => {
-    const pathRoot = `/details/${tokenSymbol}-${fundAddress}`;
+    const pathRoot = `${fundDetailsRoute}`;
     console.log(newPath);
     if (
       trimTrailingSlash(newPath) === pathRoot ||
@@ -126,14 +135,77 @@ watch(
   }
 );
 
+const switchNetwork = async (chainId: string) => {
+  isSwitchingNetworks.value = true;
+  try {
+    if (accountStore.connectedWallet) {
+      // Ask the connected user to switch network.
+      await accountStore.setActiveChain(chainId);
+    } else {
+      // Switch active chain.
+      await web3Store.init(chainId);
+    }
+
+    // Test connection, outer catch block will except exception.
+    try {
+      await web3Store.checkConnection();
+    } catch (e: any) {
+      toastStore.errorToast("Looks like there are RPC connection problems.")
+    }
+
+  } catch (error: any) {
+    // Redirect to the home page if the user cancels the network switch
+    router.push("/");
+
+    // This error code indicates that the chain has not been added to MetaMask
+    if (error.code === 4902) {
+      try {
+        // Add the network if it is not yet added.
+        // TODO: finish this for better UX, get network RPC mapping
+        // await accountStore.connectedWallet?.provider.request({
+        //   method: 'wallet_addEthereumChain',
+        //   params: [{
+        //     chainId: chainId,
+        //     rpcUrl: 'https://rpc-mainnet.maticvigil.com/',
+        //     chainName: 'Polygon Mainnet',
+        //     nativeCurrency: {
+        //       name: 'MATIC',
+        //       symbol: 'MATIC', // 2-6 characters long
+        //       decimals: 18,
+        //     },
+        //     blockExplorerUrls: ['https://polygonscan.com']
+        //   }]
+        // });
+        toastStore.errorToast(
+          "Oops, something went wrong switching networks. " +
+          "Check if the network is added to your wallet provider.",
+        )
+      } catch (addError) {
+        console.error("Failed to add the network:", addError);
+      }
+    } else {
+      toastStore.errorToast("Oops, something went wrong switching networks.")
+    }
+  }
+  isSwitchingNetworks.value = false;
+}
+
 onMounted(() => {
   fetchFund();
   setBreadcrumbItems([]);
+
+  // check if we are on correct chainId
+  if (chainId !== web3Store.chainId) {
+    switchNetwork(chainId);
+  }
+
 });
 const fund = computed(() => fundStore.fund as IFund);
 const fundDetailsRoute = computed(
-  () => `/details/${tokenSymbol}-${fundAddress}`
+  () => `/details/${chainId}-${tokenSymbol}-${fundAddress}`
 );
+
+
 
 const routes: IRoute[] = [
   {
@@ -143,9 +215,9 @@ const routes: IRoute[] = [
     text: "",
   },
   {
-    to: `/details/${tokenSymbol}-${fundAddress}/governance`,
+    to: `${fundDetailsRoute.value}/governance`,
     exactMatch: false,
-    matchPrefix: `/details/${tokenSymbol}-${fundAddress}/governance`,
+    matchPrefix: `${fundDetailsRoute.value}/governance`,
     title: "Governance",
     text: "",
   },
@@ -157,7 +229,7 @@ const routes: IRoute[] = [
     text: "",
   },
   // {
-  //   to: `/details/${tokenSymbol}-${fundAddress}/permissions`,
+  //   to: `${fundDetailsRoute.value}/permissions`,
   //   exactMatch: true,
   //   title: "Permissions",
   //   text:"",
