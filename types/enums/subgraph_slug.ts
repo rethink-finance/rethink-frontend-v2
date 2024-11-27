4bea52b8abf453cad18c23e451867de0b4367e40
L. E <luca@soonami.io>
2024-11-27 19:00:32 +0100

    fix: change subgraph slug

--- types/enums/subgraph_slug.ts
+++ types/enums/subgraph_slug.ts
@@ -1,6 +1,6 @@
 export const SubgraphSlugs: Record<string, string> = {
-  "0x1": "rethink-mainnet", // Ethereum Mainnet
-  "0xa4b1": "rethink-arbitrum-one", // Arbitrum One
-  "0x89": "rethink-matic", // Polygon (Matic)
-  "0x2105": "rethink-base", // Base (Coinbase's Layer 2)
+  "0x1": "rethinkfinance-mainnet", // Ethereum Mainnet
+  "0xa4b1": "rethinkfinance-arbitrum-one", // Arbitrum One
+  "0x89": "rethinkfinance-matic", // Polygon (Matic)
+  "0x2105": "rethinkfinance-base", // Base (Coinbase's Layer 2)
 };
