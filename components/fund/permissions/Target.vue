859b65137a0037f0407c5ab7a1e275097e017ba6
Luka Tavčer <lukatavcer@yahoo.com>
2025-01-28 21:23:15 +0100

    Fetch permissions ABI

--- components/fund/permissions/Target.vue
+++ components/fund/permissions/Target.vue
@@ -5,10 +5,52 @@
       {{ target?.address }}
     </div>
     <div>
-      Fetched ABI
-      <pre class="permissions__target_json">
-        {{ JSON.stringify(writeFunctions, null, 4) }}
-      </pre>
+      <div class="permissions__target_abi_fetch_text">
+        <template v-if="isFetchingTargetABI">
+          <v-progress-circular
+            indeterminate
+            color="gray"
+            size="22"
+            width="2"
+            class="me-2"
+          />
+          Fetching contract ABI
+        </template>
+        <template v-else-if="abiDetectedWriteFunctions.length">
+          <Icon
+            icon="weui:done-filled"
+            width="1rem"
+            height="1rem"
+            color="var(--color-success)"
+            class="mt-1"
+          />
+          Contract ABI detected
+        </template>
+        <template v-else>
+          Unable to fetch ABI for this addrses.
+          Upload contract ABI
+          <!-- TODO upload ABI form input -->
+        </template>
+      </div>
+      <template
+        v-if="abiDetectedWriteFunctions.length && !isFetchingTargetABI"
+      >
+        <div class="permissions__list">
+          <div
+            v-for="func in abiDetectedWriteFunctions"
+            :key="func.selector"
+            class="permissions__list_item"
+          >
+            <span>
+              {{ func.name }}
+            </span>
+            <span class="permissions__target_params">
+              {{ getParamsTypesTitle(func as FunctionFragment) }}
+            </span>
+          </div>
+        </div>
+        <pre class="permissions__target_abi_json">{{ JSON.stringify(abiDetectedWriteFunctions, null, 4) }}</pre>
+      </template>
     </div>
   </div>
 </template>
@@ -19,10 +61,13 @@ import type { Target } from "~/types/zodiac-roles/role";
 import type { ChainId } from "~/store/web3/networksMap";
 import type { Explorer } from "~/services/explorer";
 import { getWriteFunctions } from "~/composables/zodiac-roles/conditions";
+import { useToastStore } from "~/store/toasts/toast.store";
+import { getParamsTypesTitle } from "~/composables/zodiac-roles/target";
 
 const { $getExplorer } = useNuxtApp();
-const writeFunctions = ref<FunctionFragment[]>([]);
-
+const toastStore = useToastStore();
+const abiDetectedWriteFunctions = ref<FunctionFragment[]>([]);
+const isFetchingTargetABI = ref(false);
 const props = defineProps({
   target: {
     type: Object as PropType<Target>,
@@ -35,21 +80,36 @@ const props = defineProps({
 });
 
 const fetchTargetABI = async () => {
-  console.log("Fetch target ABI");
+  abiDetectedWriteFunctions.value = [];
   if (!props.target.address) return;
-  // TODO fetch target ABI
-  console.log("Fetch target ABI action", props.chainId);
-  const explorer: Explorer = $getExplorer(props.chainId);
-  console.log("Explorer:", explorer);
+  isFetchingTargetABI.value = true;
+  console.log("Fetch target ABI action", props.chainId, props.target.address);
 
+  let explorer: Explorer;
+  try {
+    explorer = $getExplorer(props.chainId);
+  } catch (error: any) {
+    return handleABIError(error);
+  }
+
+  console.log("Explorer:", explorer);
   try {
     const resultAbiJson = await explorer.abi(props.target.address)
     console.log("fetched ABI", resultAbiJson);
-    writeFunctions.value = getWriteFunctions(resultAbiJson);
-    console.log("fetched ABI writeFunctions", writeFunctions.value);
+    abiDetectedWriteFunctions.value = getWriteFunctions(resultAbiJson);
+    console.log("fetched ABI abiDetectedWriteFunctions", abiDetectedWriteFunctions.value);
+    isFetchingTargetABI.value = false;
   } catch (error: any) {
-    console.error(error);
+    handleABIError(error);
   }
+}
+
+const handleABIError = (error: any) => {
+  const errorMsg = "Something went wrong fetching target ABI.";
+
+  isFetchingTargetABI.value = false;
+  console.error(errorMsg, error);
+  toastStore.errorToast(errorMsg + error.message);
 }
 
 watch(
@@ -66,11 +126,31 @@ watch(
     display: flex;
     padding: 1rem;
   }
-
-  &__target_json {
+  &__target_abi_json {
     color: #dcdcaa;
     padding: 10px;
     font-size: 0.85rem;
+    white-space: pre-wrap;
+    background-color: $color-badge-navy;
+    border: 1px solid $color-gray-transparent;
+    margin-top: 2rem;
+  }
+  &__target_params {
+    color: $color-steel-blue;
+    margin-left: 0.3rem;
+  }
+  &__target_abi_fetch_text {
+    display: flex;
+    align-items: center;
+    margin: 1rem 0;
+    gap: 0.25rem;
+    padding: 1rem;
+    border: 1px solid $color-gray-transparent;
+    background-color: $color-badge-navy;
+
+  }
+  &__list_item {
+    font-family: monospace;
     white-space: pre-wrap;
   }
 }
