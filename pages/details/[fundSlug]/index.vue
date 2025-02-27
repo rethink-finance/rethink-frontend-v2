dc0f0c12fbd04c61cd65ce6757614e216f6938bd
Luka Vuk <luka.vuk0909@gmail.com>
2025-02-27 22:09:54 +0100

    show nav value

--- pages/details/[fundSlug]/index.vue
+++ pages/details/[fundSlug]/index.vue
@@ -6,25 +6,54 @@
     <!--      <FundInfo :fund="fund" />-->
     <!--    </div>-->
 
+    <!-- show just NAV value -->
+    <UiDataBar class="data_bar">
+      <div class="data_bar__item">
+        <div class="data_bar__title" :class="{'justify-center': isLoadingFetchFundNAVUpdatesActionState}">
+          <v-progress-circular
+            v-if="isLoadingFetchFundNAVUpdatesActionState"
+            class="d-flex"
+            size="18"
+            width="2"
+            indeterminate
+          />
+          <template v-else>
+            {{ fundStore.fundTotalNAVFormattedShort ?? "N/A" }}
+          </template>
+        </div>
+        <div class="data_bar__subtitle">
+          NAV
+        </div>
+      </div>
+    </UiDataBar>
+
     <div class="d-flex flex-column">
       <div class="main_card main_grid order-1 order-sm-0">
         <FundSettlement :fund="fund" :should-user-delegate="shouldUserDelegate" />
         <FundCurrentCycle :fund="fund" />
       </div>
 
-      <!--      <div class="main_card">-->
-      <!--        <FundChart :fund="fund" />-->
-      <!--      </div>-->
+    <!--      <div class="main_card">-->
+    <!--        <FundChart :fund="fund" />-->
+    <!--      </div>-->
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useActionStateStore } from "~/store/actionState.store";
 import { useFundStore } from "~/store/fund/fund.store";
+import { ActionState } from "~/types/enums/action_state";
 import type IFund from "~/types/fund";
 
 const fundStore = useFundStore();
 const { shouldUserDelegate } = storeToRefs(fundStore);
+
+const actionStateStore = useActionStateStore();
+
+const isLoadingFetchFundNAVUpdatesActionState =
+  computed(() => actionStateStore.isActionState("fetchFundNAVDataAction", ActionState.Loading));
+
 
 const fund = useAttrs().fund as IFund;
 </script>
