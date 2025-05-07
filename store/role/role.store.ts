0adfc5660c95bb666f7b6027e95e0a18dd360d59
Luka Tavčer <lukatavcer@yahoo.com>
2025-05-07 14:34:25 +0200

    Missing ABI message

--- store/role/role.store.ts
+++ store/role/role.store.ts
@@ -367,7 +367,7 @@ export const useRoleStore = defineStore("roleStore", () => {
 
     if (!func) {
       throw new Error(
-        `Missing ABI: Function with sighash ${funcCondition.sighash} requires ABI data to determine targets. Please update the contract ABI to include this function.`,
+        `Missing ABI: Function with sighash ${funcCondition.sighash} requires ABI data to determine parameter types. Please update the contract ABI to include this function.`,
       )
     }
 
