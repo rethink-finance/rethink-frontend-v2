d5a02cc2e18c22b5333fbc3bda4d8d29f23e1bb0
Luka Tavčer <lukatavcer@yahoo.com>
2025-07-15 11:24:22 +0200

    Refactor Fund Page

---
+++ components/fund/FundHeader.vue
@@ -0,0 +1,92 @@
+<template>
+  <div v-if="breadcrumbItems.length === 0" class="fund_name">
+    <v-avatar class="fund_name__avatar" :rounded="false" size="3.75rem">
+      <img
+        :src="fund.photoUrl"
+        class="fund_name__avatar_img"
+        alt="fund cover image"
+      >
+    </v-avatar>
+    <div class="fund_name__title">
+      <p>
+        {{ fund?.title }}
+      </p>
+    </div>
+    <div v-if="fund?.strategistName" class="fund_name__title">
+      <p>
+        |
+      </p>
+    </div>
+    <div
+      v-if="fund?.strategistName"
+      class="fund_name__subtitle"
+    >
+      <span>by </span>
+      <a
+        :href="fund?.strategistUrl"
+        target="_blank"
+        class="strategist_url"
+      >
+        {{ fund?.strategistName }}
+      </a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type IFund from "~/types/fund";
+import type BreadcrumbItem from "~/types/ui/breadcrumb";
+
+defineProps<{
+  fund: IFund;
+  breadcrumbItems: BreadcrumbItem[];
+}>();
+</script>
+
+<style lang="scss" scoped>
+.fund_name {
+  background-color: $color-bg-transparent;
+  border-radius: $default-border-radius;
+  padding: 0.5rem 0.62rem;
+  display: flex;
+  flex-direction: row;
+  gap: 0.5rem;
+  align-items: center;
+
+  @include sm {
+    padding: 8px;
+  }
+
+  &__avatar {
+    border: 0;
+  }
+
+  &__avatar_img {
+    border-radius: 50%;
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+  }
+
+  &__title {
+    font-weight: 700;
+    font-size: $text-md;
+  }
+
+  &__subtitle {
+    font-weight: 500;
+    font-size: $text-sm;
+    color: $color-light-subtitle;
+  }
+}
+
+.strategist_url {
+  color: $color-light-subtitle;
+  text-decoration: none;
+  transition: color 0.3s ease;
+
+  &:hover {
+    color: $color-primary;
+  }
+}
+</style>
--- pages/details/[fundSlug].vue
+++ pages/details/[fundSlug].vue
@@ -12,62 +12,15 @@
       :description="fund?.description"
       :image-url="fund?.photoUrl"
     />
-    <div v-if="breadcrumbItems.length === 0" class="fund_name">
-      <v-avatar class="fund_name__avatar" :rounded="false" size="3.75rem">
-        <img
-          :src="fund.photoUrl"
-          class="fund_name__avatar_img"
-          alt="fund cover image"
-        >
-      </v-avatar>
-      <div class="fund_name__title">
-        <p>
-          {{ fund?.title }}
-        </p>
-      </div>
-      <div v-if="fund?.strategistName" class="fund_name__title">
-        <p>
-          |
-        </p>
-      </div>
-      <div
-        v-if="fund?.strategistName"
-        class="fund_name__subtitle"
-      >
-
-        <span>by </span>
-        <a
-          :href="fund?.strategistUrl"
-          target="_blank"
-          class="strategist_url"
-        >
-          {{ fund?.strategistName }}
-        </a>
-
-      </div>
-    </div>
-    <div v-if="breadcrumbItems.length === 0" class="details_nav_container">
-      <div class="details_nav">
-        <div class="overlay-container" />
-        <nuxt-link
-          v-for="navRoute in computedRoutes"
-          :key="navRoute.to"
-          :to="navRoute.to"
-          class="link"
-        >
-          <v-btn
-            class="nav-link"
-            variant="plain"
-            :active="navRoute.isActive"
-            :color="navRoute.pathColor"
-          >
-            <div :class="{ 'title-box': navRoute.isActive }">
-              {{ navRoute.title }}
-            </div>
-          </v-btn>
-        </nuxt-link>
-      </div>
-    </div>
+    <FundHeader
+      :fund="fund"
+      :breadcrumb-items="breadcrumbItems"
+    />
+    <FundNavigation
+      v-if="breadcrumbItems.length === 0"
+      :routes="routes"
+      :fund-details-route="fundDetailsRoute"
+    />
     <UiBreadcrumbs
       v-if="breadcrumbItems.length > 0"
       :items="breadcrumbItems"
@@ -99,17 +52,17 @@
 import { useAccountStore } from "~/store/account/account.store";
 import { useActionStateStore } from "~/store/actionState.store";
 import { useFundStore } from "~/store/fund/fund.store";
-import { useSettingsStore } from "~/store/settings/settings.store";
 import { ActionState } from "~/types/enums/action_state";
 import { ChainId } from "~/types/enums/chain_id";
 import type IFund from "~/types/fund";
 import type IRoute from "~/types/route";
 import type BreadcrumbItem from "~/types/ui/breadcrumb";
+import FundNavigation from "~/components/fund/Navigation.vue";
+import FundHeader from "~/components/fund/FundHeader.vue";
 
 const accountStore = useAccountStore();
 const fundStore = useFundStore();
 const actionStateStore = useActionStateStore();
-const appSettingsStore = useSettingsStore();
 const route = useRoute();
 // fund address is always in the third position of the route
 // e.g. /details/0xa4b1-TFD3-0x1234 -> 0x1234
@@ -176,7 +129,6 @@ watch(
   },
 );
 
-
 const fundDetailsRoute = computed(
   () => `/details/${fundChainId}-${fundSymbol}-${fundAddress}`,
 );
@@ -234,158 +186,13 @@ const routes: IRoute[] = [
   },
 ];
 
-const isPathActive = (path: string = "", exactMatch = true) =>
-  exactMatch ? route?.path === path : route?.path.startsWith(path);
-const getPathColor = (isActive = false, color = "#77839f") =>
-  isActive ? "primary" : color;
-
-const computedRoutes = computed(() => {
-  const showInManageMode = [
-    `${fundDetailsRoute.value}/flows`,
-    `${fundDetailsRoute.value}/execution-app`,
-  ]
-  return routes.map((routeItem: IRoute) => {
-    const isHidden = showInManageMode.includes(routeItem.to) ? !appSettingsStore.isManageMode : false;
-
-    let isActive;
-    if (routeItem.exactMatch) {
-      isActive = isPathActive(routeItem.to, true);
-    } else if (
-      isPathActive(routeItem.matchPrefix, false) ||
-      isPathActive(routeItem.to, true)
-    ) {
-      isActive = true;
-    } else {
-      isActive = false;
-    }
-
-    return {
-      ...routeItem,
-      isActive,
-      pathColor: getPathColor(isActive, routeItem.color),
-      target: routeItem.isExternal ? "_blank" : "",
-      isHidden,
-    };
-  }).filter((routeItem: IRoute) => !routeItem.isHidden);
-});
 </script>
 
 <style lang="scss" scoped>
 .fund_details {
   width: 100%;
 }
-.details_nav {
-  position: relative;
-  padding-top: 8px;
-  width: 100%;
-}
-
-.details_nav_container {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  margin-top: 4px;
-  padding-left: 1rem;
-  padding-right: 1rem;
-  padding-bottom: 8px;
-  margin-bottom: 40px;
-
-  background-color: $color-bg-transparent;
-  border-radius: 4px;
-
-  @include sm {
-    padding-left: 0;
-    padding-right: 0;
-  }
-}
-
-.link {
-  &:first-of-type {
-    .nav-link {
-      padding-left: 8px;
-    }
-  }
-}
-
-.nav-link {
-  height: 100%;
-  text-transform: Capitalize;
-  font-size: 1rem;
-  font-weight: 700;
-  padding: 0.5rem;
-
-  &:not(:hover) {
-    opacity: 0.85;
-  }
-}
-
-.title-box {
-  position: relative;
-  border-bottom: 2px solid;
-  border-color: var(--color-primary);
-  padding-bottom: 1rem;
-}
-
-.overlay-container {
-  position: absolute;
-  bottom: 0;
-  left: 50%;
-  transform: translateX(-50%);
-
-  background-color: var(--color-divider);
-  width: calc(100% - 16px);
-  height: 2px;
-}
-
-.fund_name {
-  background-color: $color-bg-transparent;
-  border-radius: $default-border-radius;
-  padding: 0.5rem 0.62rem;
-  display: flex;
-  flex-direction: row;
-  gap: 0.5rem;
-  align-items: center;
-
-  @include sm {
-    padding: 8px;
-  }
-
-  &__avatar {
-    border: 0;
-  }
-
-  &__avatar_img {
-    border-radius: 50%;
-    height: 100%;
-    width: 100%;
-    object-fit: cover;
-  }
-
-  &__title {
-    font-weight: 700;
-    font-size: $text-md;
-  }
-
-  &__subtitle {
-    font-weight: 500;
-    font-size: $text-sm;
-    color: $color-light-subtitle;
-
-
-  }
-}
-
-.strategist_url {
-  color: $color-light-subtitle;
-  text-decoration: none;
-  transition: color 0.3s ease;
-
-  &:hover {
-    color: $color-primary;
-  }
-}
-
 .breadcrumbs {
-  margin-bottom: 32px;
+  margin-bottom: 2rem;
 }
 </style>
