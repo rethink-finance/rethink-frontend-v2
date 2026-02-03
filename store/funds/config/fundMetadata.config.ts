cae826476f1efafbedfc884f5c7da481118a7a17
Luka Tavčer <lukatavcer@yahoo.com>
2026-02-03 10:49:55 +0100

    Add texts to open alpha smart btc

--- store/funds/config/fundMetadata.config.ts
+++ store/funds/config/fundMetadata.config.ts
@@ -14,7 +14,8 @@ export const fundMetaDataHardcoded = {
       address: "0xBE0B0C435EA1156F76d3E116Fbd5606743ab179a",
       strategistName: "The DAO of Capital",
       strategistUrl: "https://discord.gg/punj9gPZP3",
-      subtitle: "BTC, ETH, stables and gold. Growth, yield, and resilience in one portfolio.",
+      subtitle:
+        "BTC, ETH, stables and gold. Growth, yield, and resilience in one portfolio.",
     },
     {
       address: "0xFc3de74eA28474FF02e378F5F8658354E481d7B9",
@@ -35,7 +36,8 @@ export const fundMetaDataHardcoded = {
       address: "0x533f164d91e3F8169a7043f7094f44af87Fb7CA4",
       strategistName: "INDEFI",
       strategistUrl: "https://indefi.io/",
-      subtitle: "Volatility-driven rebalancing with sustained exposure to ETH appreciation.",
+      subtitle:
+        "Volatility-driven rebalancing with sustained exposure to ETH appreciation.",
     },
   ],
   // Arbitrum One
@@ -44,7 +46,8 @@ export const fundMetaDataHardcoded = {
       address: "0x00a4DCBBB7Eb5d0c4Ef33Ab9763DDE5Cd91A4b10",
       strategistName: "Flex.hl",
       strategistUrl: "https://x.com/kinzflx",
-      subtitle: "Adaptive perps strategy built to survive chop and thrive in runs.",
+      subtitle:
+        "Adaptive perps strategy built to survive chop and thrive in runs.",
     },
     {
       address: "0x5E0f37920DDee57dAbAf5A73B21D51075AeDbEBE",
@@ -66,12 +69,22 @@ export const fundMetaDataHardcoded = {
       address: "0x58BA86cF363De2Bdbe57ad885B47F1B985EA9F31",
       strategistName: "carrotfunding.io",
       strategistUrl: "https://carrotfunding.io",
-      subtitle: "Trader funding vault backing consistently profitable perps traders.",
+      subtitle:
+        "Trader funding vault backing consistently profitable perps traders.",
     },
     {
       // Eliot wave
       address: "0x55f4949AF6356BBb71aeCC23eb36aBA4171Ffdaa",
-      subtitle: "Adaptive trading system that identifies market regimes and swing trade opportunities.",
+      subtitle:
+        "Adaptive trading system that identifies market regimes and swing trade opportunities.",
+    },
+    {
+      // SmartBTC
+      address: "0x202Cf222611B827B3Ae5a3Ee055e38Bae18256F5",
+      strategistName: "Open Alpha",
+      strategistUrl: "https://openalpha.vc/",
+      subtitle:
+        "Revolutionizing Investment in Web3.",
     },
   ],
   // ETH Mainnet
@@ -80,12 +93,23 @@ export const fundMetaDataHardcoded = {
       address: "0xaC3D76E29f866702E17f571cccb15937E5A17303",
       strategistName: "soonami.io",
       strategistUrl: "https://soonami.io/venture-staking",
-      subtitle: "Native ETH staking and reinvesting yield into soonami.io ecosystem.",
+      subtitle:
+        "Native ETH staking and reinvesting yield into soonami.io ecosystem.",
     },
     {
       // Boreal USD Yield
       address: "0xd245A74898124ae10DE4DB5ec842032042654F0d",
-      subtitle: "Yield on leveraged bravUSDC on Morpho, to access DeFi opportunities through arbitrage, basis trades, and liquidity provision.",
+      subtitle:
+        "Yield on leveraged bravUSDC on Morpho, to access DeFi opportunities through arbitrage, basis trades, and liquidity provision.",
     },
   ],
-} as Record<ChainId, { address: string; strategistName?: string; strategistUrl?: string; oivChatUrl?: string; subtitle?: string }[]>;
+} as Record<
+  ChainId,
+  {
+    address: string;
+    strategistName?: string;
+    strategistUrl?: string;
+    oivChatUrl?: string;
+    subtitle?: string;
+  }[]
+>;
